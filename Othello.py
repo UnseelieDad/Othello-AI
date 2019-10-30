a8b0df7a6079e1fc581ce969866fd40132827139
Seth <seth@pop-os.localdomain>
2019-10-29 19:33:10 -0500

    Turns and text set up

--- Othello.py
+++ Othello.py
@@ -1,4 +1,7 @@
+'''Othello game program.'''
+
 import pygame
+from random import randint
 
 pygame.init()
 
@@ -11,9 +14,25 @@ RES_X, RES_Y = 1120, 630
 ROWS, COLS = 8, 8
 BOARD_X, BOARD_Y = 480, 480
 SQUARE_X, SQUARE_Y = BOARD_X / 8, BOARD_Y / 8
+CORNER_X, CORNER_Y = (RES_X / 2) - (BOARD_X / 2), (RES_Y / 2) - (BOARD_Y / 2)
+FONT = font = pygame.font.Font("freesansbold.ttf", 32)
+
 
 # Global
+global isStart
+global turn
+global player1Score
+global player2Score
+global textList
+global player1Color
+global player2Color
+
 isStart = True
+turn = "b"
+player1Score = 0
+player2Score = 0
+textList = []
+
 
 # Initialize the the back of the game board and each individual square in an 8x8 grid.
 def createBoard():
@@ -26,9 +45,6 @@ def createBoard():
     # Set the center of the board to the center of the window
     boardBack.center = ((RES_X / 2), (RES_Y / 2))
 
-    # Find the board's top left corner
-    boardCornerX = (RES_X / 2) - (BOARD_X / 2)
-    boardCornerY = (RES_Y / 2) - (BOARD_Y / 2)
     offsetX = 0
     offsetY = 0
 
@@ -36,19 +52,18 @@ def createBoard():
     for row in board:
         for square in row:
             # Set the square's position based off of the board's top left corner and offset by the size of other squares
-            square[0] = square[0].move((boardCornerX + offsetX), (boardCornerY + offsetY))
-            offsetX += 60
-        
+            square[0] = square[0].move((CORNER_X + offsetX), (CORNER_Y + offsetY))
+            offsetX += 60       
         offsetX = 0
         offsetY += 60
 
     # If the game is just starting put pieces in the starting positions
-    if isStart == True:
+    if isStart is True:
         board[3][3][1] = "w"
         board[4][3][1] = "b"
         board[3][4][1] = "b"
         board[4][4][1] = "w"
-        isStart == False
+        isStart is False
 
     return board, boardBack
 
@@ -61,22 +76,14 @@ def drawBoard(board, boardBack):
     # Draw the outline for each square
     for row in board:
         for square in row:
-
             # If the mouse is hovering over the rectangle, change the outline to orange
-            if square[0].collidepoint(pygame.mouse.get_pos()) and square[1] is "e":
+            if square[1] is "h":
                 rectColor = ORANGE
-                square[1] = "h"
-
-                #if pygame.event.get().contains
-
-            elif not square[0].collidepoint(pygame.mouse.get_pos()) and square[1] is "h":
-                rectColor = BLACK
-                square[1] = "e"
-
             else:
                 rectColor = BLACK
 
             pygame.draw.rect(screen, rectColor, square[0], 1)
+ 
             # If a square isn't empty draw the appropriate piece there
             if square[1] is not "e" and square[1] is not "h":
                 drawPiece(square)
@@ -91,8 +98,77 @@ def drawPiece(square):
 
     pygame.draw.circle(screen, color, square[0].center, SQUARE_X / 3)
 
+def createText():
+
+    player1Text = FONT.render("Player 1 Score:", True, GREEN)
+    player1TextRect = player1Text.get_rect()
+    player1TextRect.center = ((CORNER_X / 2),(CORNER_Y / 2))
+
+    player2Text = FONT.render("Player 2 Score:", True, GREEN)
+    player2TextRect = player2Text.get_rect()
+    player2TextRect.center = ((BOARD_X + CORNER_X * 1.5), (CORNER_Y / 2))
+
+    if first is 1:
+        turnText = FONT.render("Player 1's Turn", True, GREEN)
+    else:
+        turnText = FONT.render("Player 2's Turn", True, GREEN)
+    turnTextRect = turnText.get_rect()
+    turnTextRect.center = ((RES_X / 2), (BOARD_Y + CORNER_Y * 1.5))
+
+    player1ScoreText = FONT.render("{}".format(player1Score), True, GREEN)
+    player1ScoreTextRect = player1ScoreText.get_rect()
+    player1ScoreTextRect.center = (player1TextRect.center[0], (player1TextRect.center[1] + 40))
+
+    player2ScoreText = FONT.render("{}".format(player2Score), True, GREEN)
+    player2ScoreTextRect = player2ScoreText.get_rect()
+    player2ScoreTextRect.center = (player2TextRect.center[0], (player2TextRect.center[1] + 40))
+
+    textList.extend([(player1Text, player1TextRect), (player2Text, player2TextRect), (turnText, turnTextRect), (player1ScoreText, player1ScoreTextRect), (player2ScoreText, player2ScoreTextRect)])
+
+def updateTurn():
+
+    #global textList
+
+    if textList[2]:
+        oldTurnText = textList.pop(2)
+
+        if turn is player1Color:
+            newTurnText = (FONT.render("Player 1's Turn", True, GREEN), oldTurnText[1])
+        else:
+            newTurnText = (FRONT.render("Player 2's Turn", True, GREEN), oldTurnText[1])
+
+        textList.insert(2, newTurnText)
+
+
+    
+
+def updateScore():
+
+    #global textList    
+    global textList
+
+    # If there are scores in the list remove them.
+    if len(textList) > 3:
+        textList = textList[:-2]
+
+    textList.extend([(player1ScoreText, player1ScoreTextRect), (player2ScoreText, player2ScoreTextRect)])
+
+def drawText():
+
+    for text in textList:
+        screen.blit(text[0], text[1])
+
 
 # Main       
+
+# See who's going first
+first = randint(1,2)
+if first is 1:
+    player1Color = "b"
+    player2Color = "w"
+else:
+    player2Color = "b"
+    player1Color = "w"
 
 # Create the main window
 screen = pygame.display.set_mode((RES_X, RES_Y))
@@ -101,20 +177,49 @@ done = False
 # Create the board
 board, boardBackground = createBoard()
 
+# Create text
+createText()
+
 # Main game loop
 while not done:
     
+    # Main events loop
     # If the x button on the window is clicked, end the game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    # Wipe the screen        
+        # If the mouse moves or is clicked
+        if event.type is pygame.MOUSEMOTION or event.type is pygame.MOUSEBUTTONUP:
+            # Get the mouse's position
+            pos = pygame.mouse.get_pos()
+            # For each square on the board
+            for row in board:
+                for square in row:
+                    # If the mouse is over the square and the square is empty
+                    if square[0].collidepoint(pos) and square[1] is not "b" and square[1] is not "w":
+                        # if it was a click place a piece
+                        if event.type is pygame.MOUSEBUTTONUP:
+                            if turn is "b": 
+                                square[1] = "b"
+                                turn = "w"
+                            else:
+                                square[1] = "w"
+                                turn = "b"
+                        # If the mouse moved, highlight the square
+                        if event.type is pygame.MOUSEMOTION:
+                            square[1] = "h"
+                    # Un-highlight any square the mouse doesn't touch
+                    elif square[1] is "h":
+                        square[1] = "e"
+
+    # Wipe the screen
     screen.fill(BLACK)
 
     # Draw the board and pieces
     drawBoard(board, boardBackground)
 
+    drawText()
     
     # Update the displaly
     pygame.display.flip()
